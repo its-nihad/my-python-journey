6d2dc7d26b835274e459fbfc7bf2b65c1e722f82
its-nihad <nihadmaranchery450@gmail.com>
2026-08-25 23:21:34 +0530

    Add files via upload

---
+++ project-01.py
@@ -0,0 +1,10 @@
+# INDRODUCING MY SELF
+
+name="MOHAMED NIHAD V"
+qualification="Bcom Graduate"
+current="working at MAX FASHION as CRE & Cashier and also pursuing Data Science at WESTBERG INTERNATIONAL"
+cre="Customer Relationship Executive"
+print("Hey, Iam ",name)
+print("iam a ",qualification)
+print("Now iam ",current)
+print("note: CRE means",cre)
