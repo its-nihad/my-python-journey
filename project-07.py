e0422017ef7a6deba6a85b204157bfe1265ad290
its-nihad <nihadmaranchery450@gmail.com>
2026-09-04 23:29:08 +0530

    Add files via upload

---
+++ project-07.py
@@ -0,0 +1,10 @@
+# to find simple interest
+
+p=int(input("enter the principal amount:"))
+t=int(input("enter the time period:"))
+r=int(input("enter the rate of interest:"))
+
+si=p*t*r/100
+
+print("interest is ₹",si)
+
