49b579f9e2427ff204391948d0c81527e0f34c67
its-nihad <nihadmaranchery450@gmail.com>
2026-09-06 12:52:09 +0530

    Add files via upload

---
+++ project-08.py
@@ -0,0 +1,11 @@
+# to check the given number is postive or negative
+
+num=int(input("enter the number to check:"))
+if num>0:
+    print(num,"is postive")
+elif num<0:
+    print(num,"is negative")
+else:
+    print("zero")
+
+    
