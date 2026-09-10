afdd9ae36de6055170c69da3c6fcaab6b556830c
its-nihad <nihadmaranchery450@gmail.com>
2026-09-10 11:54:19 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,34 @@
+#CONDITIONAL STATEMENTS
+
+#1. if statement
+
+#syntax
+
+#if condition:
+    #code block
+
+
+#2. if else statement
+
+#syntax
+
+'''
+if condition:
+    #code block
+else:
+    #code block
+'''
+
+
+#3 if elif else statement
+
+#syntax
+
+'''
+if condition:
+    #code block
+elif condition:
+    #code block
+else:
+    #code block
+'''
