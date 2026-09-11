3947a669367ff1341c5853c39cc78f65e3931c9a
its-nihad <nihadmaranchery450@gmail.com>
2026-09-11 23:53:46 +0530

    Add files via upload

---
+++ project-11.py
@@ -0,0 +1,11 @@
+# find the largest among the given three numbers
+
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:"))
+n3=int(input("enter the third number:"))
+if n1>=n2 and n1>=n3:
+    print(n1,"is largest")
+elif n2>=n1 and n2>=n3:
+    print(n2,"is largest")
+else:
+    print(n3,"is largest")
