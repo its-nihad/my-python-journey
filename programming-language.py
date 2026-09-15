a27e4e15e7f51b0b8462a19aecc370d2241cea74
its-nihad <nihadmaranchery450@gmail.com>
2026-09-15 16:00:31 +0530

    Add files via upload

---
+++ programming-language.py
@@ -0,0 +1,37 @@
+#EVOLUTION OF PROGRAMMING LANGUAGE
+
+'''
+
+1.MACHINE LEVEL LANGUAGE
+
+* (0,1)
+* Algebric form
+*Forton
+eg-:
+    net salary= 1,00,000
+    new salary= net salary+10,000
+
+*COBOL - (Common Business Oriented Language)
+eg-:
+    add tenthousand to net salary and assign new salary
+
+2.ASSEMBLY LEVEL LANGUAGE
+
+*(0,1) Machine level language replaced by numonize(word)
+eg-: ADD,SUB
+*Assembler - translate the assembly level language into machine level language
+
+3.HIGH LEVEL PROGRAMMING LANGUAGE
+
+*it is similar to human language
+eg-: C,C++,java script,python
+
+
+LANGUAGE TRANSLATORS (COMPLIANCE)
+
+1.compailer - C,C++ ---> executed complete code at single click
+2.interpreter - python ---> execute line by line (helps to find mistake)
+
+'''
+
+
