aa36e990f6a7a838cba7e6adf7257915f1f6991f
its-nihad <nihadmaranchery450@gmail.com>
2026-09-17 12:20:49 +0530

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,29 @@
+#DATA TYPES
+
+'''
+
+1.NUMERICAL
+
+*intigier(int)-:
+    whole number
+    eg-: 1,2,3,4.....
+*float-:
+    decimal part number
+    eg-: 2.3,6.8,8.2,0.5
+
+2.CATEGORICAL
+
+*string(str)-:
+    eg-: "python","data","java"
+
+3.BOOLEAN
+
+*True/False
+
+
+'''
+
+#variable- the name given to the storing location   (eg-:num=5,a=10)
+#identifier- the name given to any programming element (eg-:variable,function name)
+#keyword- reserved words (eg-: if,for,while....)
+
