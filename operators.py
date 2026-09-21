3eaf542bc2c50a023500c24541b63cc030e447ba
its-nihad <nihadmaranchery450@gmail.com>
2026-09-21 11:49:57 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,56 @@
+#OPERATORS
+
+#1.Arithmetic operator
+'''
+1.addition(+) ---> to add
+2.subtraction(-) ---> to minus
+3.multiplication(*) ---> to multiply
+4.division(/) ---> to divide
+5.modulus(%) ---> to find the reminder
+6.floor division(//) ---> to ignore the decimal part and take the intigier
+7.exponensial(**) ---> to write superscript
+
+note: the output is arithmetic value
+'''
+
+#2.Relational or Comparisonal operator
+'''
+1.greaterthan(>)
+2.lessthan(<)
+3.greaterthan or equal too(>=)
+4.lessthan or equal too(<=)
+5.equal too(==)
+6.not equal too(!=)
+
+note:the output is boolean value
+'''
+
+#3.Logical operators
+'''
+1.and ---> satisfy two condition,two condition should be true
+2.or ---> satisfy any one condition, any one condition should be true
+3.not ---> not True ---> False 
+           not False ---> True 
+           
+note: the output is boolean value
+'''
+
+#4.Membership operators
+'''
+1.in
+eg-:'a' in "aeiou" --->True
+    'x' in "aeiou" --->False
+2.not in 
+eg-:'x' not in "aeiou" --->True
+
+*checking it is in the family/group or not
+'''
+
+#5. Assignment operators
+'''
+* assign the value from left to right
+* "=" is the assignment operator
+eg-: a=5
+     b=10
+'''
+
