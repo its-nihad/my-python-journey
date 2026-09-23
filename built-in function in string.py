051796962a5135139a018b3c421d26e7a8959525
its-nihad <nihadmaranchery450@gmail.com>
2026-09-23 11:18:52 +0530

    Add files via upload

---
+++ built-in function in string.py
@@ -0,0 +1,64 @@
+#BUILD-IN FUNCTIONS IN STRING
+
+#sentance="the python programming is simple)
+
+#1.len()
+'''
+*to find the length of a given string
+eg:l=len(sentance)
+   print(l)
+'''
+
+#2.upper()
+'''
+*to convert the  data into uppercase
+eg-:print(sentance.upper())
+'''
+
+#3.lower()
+'''
+*to convert the data into lowercase
+eg:print(sentance.lower())
+'''
+
+#4.title()
+'''
+*to capitalize first letter of words
+eg:print(sentance.title())
+'''
+
+#5.count()
+'''
+*used to find how many times a specific character is repeated in a data
+eg:print(sentance.count("a"))
+'''
+
+#6.index()
+'''
+*used to find the position of a specific character
+eg:print(sentance.index("p"))
+'''
+
+#7.isalpha()
+'''
+*used to check the data is alphabet or not
+eg:print(sentance.isalpha())
+'''
+
+#8.isdigit()
+'''
+*used to check the data is digit or not
+eg:print(sentance.isdigit())
+'''
+
+#9.islower()
+'''
+*used to check the data is in lowercase 
+eg:print(sentance.islower())
+'''
+
+#10.isupper()
+'''
+*used to check the data is in uppercase
+eg:print(sentance.isupper())
+'''
